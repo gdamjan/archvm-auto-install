36799f635210822e457ed1214e527997fb4f4f48
Дамјан Георгиевски <gdamjan@gmail.com>
2023-07-08 17:07:09 +0200

    runtime directory is ./run/
    download and create all files in ./run/,
    makes it easier to ignore it in .gitignore

--- start.sh
+++ start.sh
@@ -3,30 +3,36 @@ set -euo pipefail
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
+ARCHISO_URL=https://geo.mirror.pkgbuild.com/iso/latest/archlinux-x86_64.iso
+
 OVMF_DIR=/usr/share/edk2-ovmf/x64
 OVMF_BIOS=$OVMF_DIR/OVMF_CODE.fd
 OVMF_VARS=$OVMF_DIR/OVMF_VARS.fd
-ARCHISO_URL=https://geo.mirror.pkgbuild.com/iso/latest/archlinux-x86_64.iso
 
 IMDS_URL=http://10.0.2.2:8000/
+ISO=./run/archlinux-x86_64.iso
+IMG=./run/arch-vm.img
+VARS=./run/arch-vm.vars.fd
 
-if [ ! -f archlinux-x86_64.iso ]; then
-  curl -O $ARCHISO_URL
+mkdir -p ./run
+if [ ! -f $ISO ]; then
+  curl -o $ISO $ARCHISO_URL
 fi
-if [ ! -f arch-vm.img ]; then
-  qemu-img create -f qcow2 arch-vm.img 10G
+if [ ! -f $IMG ]; then
+  qemu-img create -f qcow2 $IMG 10G
 fi
-if [ ! -f OVMF_VARS.fd ]; then
-  cp $OVMF_VARS .
+if [ ! -f $VARS ]; then
+  cp $OVMF_VARS $VARS
 fi
 
+# IMDS server - https://cloudinit.readthedocs.io/en/latest/reference/datasources/nocloud.html
 python -m http.server --directory imds &
 
 qemu-system-x86_64 \
   -drive if=pflash,format=raw,readonly=on,file=$OVMF_BIOS \
-  -drive if=pflash,format=raw,file=./OVMF_VARS.fd \
-  -drive file=arch-vm.img,format=qcow2 \
-  -cdrom archlinux-x86_64.iso \
+  -drive if=pflash,format=raw,file=$VARS \
+  -drive file=$IMG,format=qcow2 \
+  -cdrom $ISO \
   -smbios type=1,serial=ds="nocloud-net;s=$IMDS_URL" \
   -device virtio-net-pci,netdev=net0 \
   -netdev user,id=net0,hostfwd=tcp::2222-:22 \
