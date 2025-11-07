e71b813895a8db7e79040d06afccb3391b54e3ba
Дамјан Георгиевски <gdamjan@gmail.com>
2025-11-07 01:55:33 +0100

    use an shell array for qemu options

--- start.sh
+++ start.sh
@@ -15,6 +15,7 @@ IMG=./run/arch-vm.img
 VARS=./run/arch-vm.vars.fd
 
 DISK_SERIAL=deadbeef
+CID=3
 
 mkdir -p ./run
 if [ ! -f $ISO ]; then
@@ -30,16 +31,22 @@ fi
 # IMDS server - https://cloudinit.readthedocs.io/en/latest/reference/datasources/nocloud.html
 python -m http.server --directory imds &
 
-qemu-system-x86_64 \
-  -drive if=pflash,format=raw,readonly=on,file=$OVMF_BIOS \
-  -drive if=pflash,format=raw,file=$VARS \
-  -blockdev driver=qcow2,node-name=hd0,file.driver=file,file.filename=$IMG  \
-  -device virtio-blk-pci,drive=hd0,serial=$DISK_SERIAL \
-  -cdrom $ISO \
-  -smbios "type=1,serial=ds=nocloud-net;s=$IMDS_URL" \
-  -fw_cfg "name=opt/imds-url,string=$IMDS_URL" \
-  -fw_cfg "name=opt/root-disk-serial,string=$DISK_SERIAL" \
-  -device vhost-vsock-pci,guest-cid=3 \
-  -device virtio-net-pci,netdev=net0 \
-  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
-  -smp 2 -m 4G -machine type=q35,accel=kvm
+qemu_options=(
+  -cdrom "$ISO"
+  -drive if=pflash,format=raw,readonly=on,file=$OVMF_BIOS
+  -drive if=pflash,format=raw,file=$VARS
+  -blockdev driver=qcow2,node-name=hd0,file.driver=file,file.filename="$IMG"
+  -device virtio-blk-pci,drive=hd0,serial=$DISK_SERIAL
+  -smbios "type=1,serial=ds=nocloud-net;s=$IMDS_URL"
+  -fw_cfg "name=opt/imds-url,string=$IMDS_URL"
+  -fw_cfg "name=opt/root-disk-serial,string=$DISK_SERIAL"
+  -device vhost-vsock-pci,guest-cid=$CID
+  -device virtio-net-pci,netdev=net0
+  -netdev user,id=net0,hostfwd=tcp::2222-:22
+  -cpu max
+  -smp 2
+  -m 4G
+  -machine type=q35,accel=kvm
+)
+
+qemu-system-x86_64 "${qemu_options[@]}"
